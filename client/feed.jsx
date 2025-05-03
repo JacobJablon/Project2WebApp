3800356b0989b84b1e42259c6c3cb2a93998115a
Jacob Jablon <jrj8250@rit.edu>
2025-05-03 18:01:50 -0400

    copied in files from domo e

---
+++ client/feed.jsx
@@ -0,0 +1,64 @@
+const helper = require('./helper.js');
+const React = require('react');
+const { useState, useEffect } = React;
+const { createRoot } = require('react-dom/client');
+
+const DomoList = (props) => {
+    const [domos, setDomos] = useState(props.domos);
+
+    useEffect(() => {
+        const loadDomosFromServer = async () => {
+            const response = await fetch('/getAllDomos');
+            const data = await response.json();
+            setDomos(data.domos);
+        };
+        loadDomosFromServer();
+    }, [props.reloadDomos]);
+
+    if(domos.length === 0) {
+        return (
+            <div className="domoList">
+                <h3 className="emptyDomo">No Domos Yet!</h3>
+            </div>
+        );
+    }
+
+    const domoNodes = domos.map(domo => {
+        if (!Object.hasOwn(domo, "emotion")) {
+            domo.emotion = "No emotion"
+        }
+        return (
+            <div key={domo.id} className="domo">
+                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+                <h3 className="domoName">Name: {domo.name}</h3>
+                <h3 className="domoAge">Age: {domo.age}</h3>
+                <h3 className="domoEmotion">Emotion: {domo.emotion}</h3>
+            </div>
+        );
+    });
+
+    return (
+        <div className="domoList">
+            {domoNodes}
+        </div>
+    );
+};
+
+const App = () => {
+    const [reloadDomos, setReloadDomos] = useState(false);
+
+    return (
+        <div>
+            <div id="domos">
+                <DomoList domos={[]} reloadDomos={reloadDomos} />
+            </div>
+        </div>
+    );
+};
+
+const init = () => {
+    const root = createRoot(document.getElementById('app'));
+    root.render( <App /> );
+};
+
+window.onload = init;
